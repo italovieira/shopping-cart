e04c7d09ec508e487fedb74fa39c90935d68806a
Ítalo Vieira <italogomesv@gmail.com>
2020-07-29 19:05:30 -0300

    Make Header component more responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import styles from './Header.module.css'
 
 const Header = () => {
   return (
-    <section className={styles.container}>
-      <div className={styles.title}>Shopping</div>
-      <div className={styles.name}>John doe</div>
+    <section className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.title}>Shopping</div>
+        <div className={styles.name}>John doe</div>
+      </div>
     </section>
   )
 }
